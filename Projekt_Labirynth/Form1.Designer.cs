8747f7c57256b3f014d834a54f0f5ebc82e10e68
makumaklowicz <mistermaniuspl@o2.pl>
2021-12-26 10:22:24 +0100

    Added: Loading image to picturebox by clicking button

--- Projekt_Labirynth/Form1.Designer.cs
+++ Projekt_Labirynth/Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Viewport = new System.Windows.Forms.PictureBox();
+            this.Load_IMG_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Viewport)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Viewport
+            // 
+            this.Viewport.BackColor = System.Drawing.Color.White;
+            this.Viewport.Location = new System.Drawing.Point(287, 133);
+            this.Viewport.Name = "Viewport";
+            this.Viewport.Size = new System.Drawing.Size(596, 474);
+            this.Viewport.TabIndex = 0;
+            this.Viewport.TabStop = false;
+            // 
+            // Load_IMG_button
+            // 
+            this.Load_IMG_button.Location = new System.Drawing.Point(375, 30);
+            this.Load_IMG_button.Name = "Load_IMG_button";
+            this.Load_IMG_button.Size = new System.Drawing.Size(440, 73);
+            this.Load_IMG_button.TabIndex = 1;
+            this.Load_IMG_button.Text = "Load Image";
+            this.Load_IMG_button.UseVisualStyleBackColor = true;
+            this.Load_IMG_button.Click += new System.EventHandler(this.Load_IMG_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1210, 750);
+            this.Controls.Add(this.Load_IMG_button);
+            this.Controls.Add(this.Viewport);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Viewport)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Viewport;
+        private System.Windows.Forms.Button Load_IMG_button;
     }
 }
 
--- Projekt_Labirynth/Form1.cs
+++ Projekt_Labirynth/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace Projekt_Labirynth
 {
     public partial class Form1 : Form
     {
+
+        Image<Bgr, byte> viewport_Buffer; 
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Load_IMG_button_Click(object sender, EventArgs e)
+        {
+            Mat temp = new Mat();
+            temp = CvInvoke.Imread(@"C:\Users\miste\Desktop\Projekt_Wizyjne_Labirynth\Labirynth.png");
+            CvInvoke.Resize(temp, temp, Viewport.Size);
+            viewport_Buffer = temp.ToImage<Bgr, byte>();
+            Viewport.Image = viewport_Buffer.Bitmap;
         }
     }
 }
